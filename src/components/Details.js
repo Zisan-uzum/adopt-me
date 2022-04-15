e146ace6b3d11c675b3d3afa5f1beccae917ad74
Zisan-uzum <uzummzisann@gmail.com>
2022-04-15 19:43:21 +0300

    add carousel smaller images

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,25 +1,33 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 const Details = () => {
   const { id } = useParams();
   const [pet, setPet] = useState({});
   const [loading, setLoading] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [carousel, setCarousel] = useState("");
 
   useEffect(() => {
     const loadPets = async () => {
       try {
         setLoading(true);
+        console.log("loading");
         const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
         const json = await res.json();
         setPet(json.pets[0]);
+        setCarousel(json.pets[0].images[0]);
         setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
-
     loadPets();
   }, [id]);
+
+  useEffect(() => {
+    pet.images && setCarousel(pet.images[index]);
+  }, [index]);
 
   return (
     <div>
@@ -29,9 +37,17 @@ const Details = () => {
         <div className="details">
           <div>
             <div className="carousel">
-              {pet.images && <img src={pet.images[0]} alt="animal" />}
+              {pet.images && <img src={carousel} alt="animal" />}
               <div className="carousel-smaller">
-                {<img src="pet.images[1]"></img>}
+                {pet.images &&
+                  pet.images.map((element, ind) => (
+                    <img
+                      onClick={() => setIndex(ind)}
+                      key={ind}
+                      src={element}
+                      alt="animal"
+                    />
+                  ))}
               </div>
             </div>
             <h1>{pet.name}</h1>
@@ -44,4 +60,5 @@ const Details = () => {
     </div>
   );
 };
+
 export default Details;
